d1128ca820972ef4357582e9421918a6e606d770
sirx2713 <bradx2713@gmail.com>
2024-03-30 15:54:51 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/30.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
